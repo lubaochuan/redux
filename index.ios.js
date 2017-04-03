3409ca5b038e2f5ed6b8ca3106ac55d203a1b8bd
lubaochuan <lubaochuan@gmail.com>
2017-04-03 06:12:50 -0500

    use redux-persist to persist state to local storage

--- index.ios.js
+++ index.ios.js
@@ -1,15 +1,26 @@
 import React from 'react'
-import { AppRegistry } from 'react-native'
+import { AppRegistry, View, AsyncStorage } from 'react-native'
 import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import { persistStore, autoRehydrate } from 'redux-persist'
 
 // Import the reducer and create a store
 import { reducer } from './todoListRedux'
-const store = createStore(reducer)
+
+// Add the autoRehydrate middleware to your redux store
+const store = createStore(reducer, undefined, autoRehydrate())
+
+// Enable persistence
+persistStore(store, {storage: AsyncStorage})
 
 // Import the App container component
 import App from './App'
 
-// Pass the store into the app container
-const AppWithStore = () => <App store={store} />
+// Pass the store into the Provider
+const AppWithStore = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
 
-AppRegistry.registerComponent('Redux', () => AppWithStore);
+AppRegistry.registerComponent('Redux', () => AppWithStore)
