c534fecba741419474459c2df34b8c9d3f0d07cc
lubaochuan <lubaochuan@gmail.com>
2017-04-03 04:06:36 -0500

    add ToDo list example using component state

--- index.ios.js
+++ index.ios.js
@@ -1,42 +1,6 @@
-// example from http://www.reactnativeexpress.com/redux
-import React from 'react'
-import { AppRegistry, Text } from 'react-native'
-import { createStore } from 'redux'
+import { AppRegistry } from 'react-native'
 
-// Define action types
-const types = {
-  INCREMENT: 'INCREMENT',
-}
-
-// Define a reducer
-const reducer = (state, action) => {
-  if (action.type === types.INCREMENT) {
-    return {count: state.count + 1}
-  }
-  return state
-}
-
-// Define the initial state of our store
-const initialState = {count: 0}
-
-// Create a store, passing our reducer function and our initial state
-const store = createStore(reducer, initialState)
-
-
-/// We're done! Redux is all set up. Here's how we can use it:
-
-
-// Now we can dispatch actions, which are understood by our store/reducer
-store.dispatch({type: types.INCREMENT})
-store.dispatch({type: types.INCREMENT})
-store.dispatch({type: types.INCREMENT})
-
-// Calling `store.getState()` returns our state object
-const App = () => (
-  <Text style={{fontSize: 100}}>
-    {store.getState().count}
-  </Text>
-)
-
+// Import the App container component
+import App from './App'
 
 AppRegistry.registerComponent('Redux', () => App);
